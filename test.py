e58d8441cc455e77e45f6308031a22511dbfbfdb
yuta15 <70703081+yuta15@users.noreply.github.com>
2024-03-02 05:46:10 +0000

    change Environment Secret variable

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ nasa_api_key = os.environ.get('NASA_API_KEY')
 api_url = 'https://api.nasa.gov/planetary/apod?api_key={}'.format(nasa_api_key)
 
 
+
 def use_requests(api_url):
 
     response = requests.get(api_url)

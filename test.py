e88cf3609ff458c1f64e37f7bc8c259073e4133a
yuta15 <70703081+yuta15@users.noreply.github.com>
2024-03-02 05:34:49 +0000

    debug

--- test.py
+++ test.py
@@ -4,8 +4,10 @@ import webbrowser
 
 import requests
 
+
 nasa_api_key = os.environ.get('NASA_API_KEY')
 api_url = 'https://api.nasa.gov/planetary/apod?api_key={}'.format(nasa_api_key)
+
 
 def use_requests(api_url):
 

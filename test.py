54928053dd28d28da29e25b9cadffa7464d00ad3
yuta15 <70703081+yuta15@users.noreply.github.com>
2024-03-02 05:47:23 +0000

    check nasa_api_key

--- test.py
+++ test.py
@@ -8,15 +8,15 @@ import requests
 nasa_api_key = os.environ.get('NASA_API_KEY')
 api_url = 'https://api.nasa.gov/planetary/apod?api_key={}'.format(nasa_api_key)
 
+print(nasa_api_key)
+
+# def use_requests(api_url):
+
+#     response = requests.get(api_url)
+#     json_response = json.loads(response.text)
+#     # photo_url = json_response['date']
+#     return json_response
 
 
-def use_requests(api_url):
 
-    response = requests.get(api_url)
-    json_response = json.loads(response.text)
-    # photo_url = json_response['date']
-    return json_response
-
-
-
-print(use_requests(api_url))
+# print(use_requests(api_url))

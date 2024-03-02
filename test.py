2603bd4b437b40274ae73e203c5c5f871a4a684a
yuta15 <70703081+yuta15@users.noreply.github.com>
2024-03-02 05:29:24 +0000

    NASA API commit

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-greeting = "Hello GitHub!!!"
-testls = ["apple", "banana", "orange"]
+import json
+import os
+import webbrowser
 
-print(greeting)
-for i in testls:
-    print(i)
+import requests
+
+nasa_api_key = os.environ.get('NASA_API_KEY')
+api_url = 'https://api.nasa.gov/planetary/apod?api_key={}'.format(nasa_api_key)
+
+def use_requests(api_url):
+
+    response = requests.get(api_url)
+    json_response = json.loads(response.text)
+    photo_url = json_response['url']
+    return photo_url
+
+
+
+print(use_requests(api_url))
